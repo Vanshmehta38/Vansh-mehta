25241cc8988ea7d03055d2678de56a37e99c7f52
vansh <vansh_mehta@codenticsoftware.com>
2024-07-27 20:54:36 +0530

    Experience section add

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// ** Icon imports
+import { FaCheck } from "react-icons/fa";
+import { MdOutlineDateRange } from "react-icons/md";
+import { FaAward } from "react-icons/fa6";
+
+const Experience = () => {
+  return (
+    <>
+      <div className="section stats-container parallax-mf bg-image">
+        <div className="overlay-mf"></div>
+        <div className="stat-item">
+          <div className="counter-ico">
+            <span className="ico-circle">
+              <FaCheck className="icon" />
+            </span>
+          </div>
+          <div className="stat-number">26+</div>
+          <div className="stat-description">Projects Completed</div>
+        </div>
+        <div className="stat-item">
+          <div className="counter-ico">
+            <span className="ico-circle">
+              <MdOutlineDateRange className="icon" />
+            </span>
+          </div>
+          <div className="stat-number">1+</div>
+          <div className="stat-description">Years of Experience</div>
+        </div>
+        <div className="stat-item">
+          <div className="counter-ico">
+            <span className="ico-circle">
+              <FaAward className="icon" />
+            </span>
+          </div>
+          <div className="stat-number">2+</div>
+          <div className="stat-description">Awards Won</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Experience;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,23 @@ import NavBar from "../components/navbar/NavBar";
 import Resume from "../components/resume/Resume";
 import Skills from "../components/skills/Skills";
 import Work from "../components/work/Work";
+import Experience from "../components/experience/Experience";
 
 const Home = () => {
+  // ** State
   const [darkMode, setDarkMode] = useState(false);
+
+  // ** var
   function toggleDarkMode() {
     setDarkMode((prevDarkMode) => !prevDarkMode);
   }
+
   return (
     <>
       <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Experience darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
